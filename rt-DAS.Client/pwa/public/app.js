4775f12e49aad9c79e1d9aa24fbc84c8b123f828
Vu Truong Giang <giangthedev@outlook.com.vn>
2022-08-30 10:12:52 +0700

    refactor directory

--- rt-DAS.Client/pwa/public/app.js
+++
@@ -1,5 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js')
-        .then((reg) => console.log('service Worker registered!',reg))
-        .catch((err) => console.log('error, sw not registered!',err))
-}
