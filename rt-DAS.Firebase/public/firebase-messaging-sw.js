1534cd6b11926ad895bcc8df566a6c3bbf713175
Ivan <88701988+ivanthepevt@users.noreply.github.com>
2022-09-09 07:07:19 +0700

    Add text to speech

--- rt-DAS.Firebase/public/firebase-messaging-sw.js
+++ rt-DAS.Firebase/public/firebase-messaging-sw.js
@@ -46,13 +46,5 @@ const messaging = firebase.messaging();
 // https://firebase.google.com/docs/cloud-messaging/concept-options
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
 });
